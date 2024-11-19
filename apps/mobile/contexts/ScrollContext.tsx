2d0e8f6b5d0e4f79009f0575ffa9a961f64265da
pablof7z <pfer@me.com>
2024-11-19 16:06:23 +0000

    choose article type

---
+++ apps/mobile/contexts/ScrollContext.tsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useRef } from 'react';
+
+const ScrollContext = createContext<any>(null);
+
+export function ScrollProvider({ children }: { children: React.ReactNode }) {
+    const scrollRef = useRef(null);
+    return (
+        <ScrollContext.Provider value={scrollRef}>
+            {children}
+        </ScrollContext.Provider>
+    );
+}
+
+export function useScroll() {
+    return useContext(ScrollContext);
+}
