668a807a12678bd45a8a9077ed4038a90b52711c
pablof7z <pfer@me.com>
2024-11-19 16:26:00 +0000

    fix nip46 login

--- apps/mobile/ndk-expo/providers/ndk/signers/nip46.ts
+++ apps/mobile/ndk-expo/providers/ndk/signers/nip46.ts
@@ -1,10 +1,16 @@
-import NDK, { NDKPrivateKeySigner, NDKNip46Signer } from '@nostr-dev-kit/ndk';
+import NDK, { NDKPrivateKeySigner, NDKNip46Signer, NDKSigner } from '@nostr-dev-kit/ndk';
 
-export async function withNip46(ndk: NDK, token: string, sk?: string) {
+export async function withNip46(ndk: NDK, token: string, sk?: string): Promise<NDKSigner | null> {
     let localSigner = NDKPrivateKeySigner.generate();
     if (sk) {
         localSigner = new NDKPrivateKeySigner(sk);
     }
 
-    return new NDKNip46Signer(ndk, token, localSigner);
+    const signer = new NDKNip46Signer(ndk, token, localSigner);
+
+    return new Promise((resolve, reject) => {
+        signer.blockUntilReady().then(() => {
+            resolve(signer);
+        }).catch(reject);
+    });
 }
