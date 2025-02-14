943c23fab8c6ed1e0a465936c59aa16d420f739f
pablof7z <pfer@me.com>
2025-02-14 15:37:14 +0000

    improve animation of incoming zap

--- apps/mobile/components/Headers/Home/index.tsx
+++ apps/mobile/components/Headers/Home/index.tsx
@@ -1,10 +1,8 @@
-import { useColorScheme } from "@/lib/useColorScheme";
 import { View, Pressable, StyleSheet, Animated } from "react-native";
 import CalendarButton from "./CalendarButton";
 import Feed from "./Feed";
 import NotificationsButton from "./NotificationsButton";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Zap } from "lucide-react-native";
 import { NDKKind, NDKNutzap, useSubscribe, useNDKCurrentUser, useUserProfile, useNDKWallet, useNDKNutzapMonitor } from "@nostr-dev-kit/ndk-mobile";
 import * as User from "@/components/ui/user";
 import { formatMoney } from "@/utils/bitcoin";
@@ -13,30 +11,17 @@ import { useState, useEffect } from "react";
 
 export default function HomeHeader() {
     const insets = useSafeAreaInsets();
-    const currentUser = useNDKCurrentUser();
+    const [showZap, setShowZap] = useState(false);
     const [nutzap, setNutzap] = useState<NDKNutzap | null>(null);
     const animationProgress = useState(new Animated.Value(0))[0];
-    const { events: nutzapEvents } = useSubscribe<NDKNutzap>( currentUser ? 
-        [{
-            kinds: [NDKKind.Nutzap],
-            "#p": [currentUser?.pubkey],
-            limit: 1
-        }] : false, { wrap: true, groupable: false, closeOnEose: true }, [currentUser?.pubkey])
 
     useEffect(() => {
         Animated.timing(animationProgress, {
-            toValue: nutzap ? 1 : 0,
+            toValue: showZap ? 1 : 0,
             duration: 300,
             useNativeDriver: true,
         }).start();
-    }, [nutzap]);
-
-    const incoming = () => {
-        setNutzap(Array.from(nutzapEvents)?.[0]);
-        setTimeout(() => setNutzap(null), 5000);
-    }
-
-    // const nutzap = Array.from(nutzapEvents)[0];
+    }, [showZap]);
 
     const { nutzapMonitor } = useNDKNutzapMonitor();
     
@@ -44,8 +29,12 @@ export default function HomeHeader() {
         if (!nutzapMonitor) return;
 
         nutzapMonitor.on("seen", (nutzap) => {
+            setShowZap(true);
             setNutzap(nutzap);
-            setTimeout(() => setNutzap(null), 1500);
+            setTimeout(() => {
+                setShowZap(false);
+                setTimeout(() => setNutzap(null), 1000);
+            }, 1500);
         });
     }, [!!nutzapMonitor]);
 
@@ -64,9 +53,6 @@ export default function HomeHeader() {
                     <Feed />
 
                     <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
-                        <Pressable onPress={incoming}>
-                            <Zap size={24} color="white" />
-                        </Pressable>
                         <CalendarButton />
                         <NotificationsButton />
                     </View>
@@ -97,7 +83,6 @@ export default function HomeHeader() {
 function IncomingZap({ nutzap }: { nutzap: NDKNutzap }) {
     const insets = useSafeAreaInsets();
     const { userProfile } = useUserProfile(nutzap.pubkey);
-    const { colors } = useColorScheme();
     
     return <View style={[zapNotificationStyle.container, { paddingTop: insets.top + 10 }]} className="pb-2">
         <User.Avatar pubkey={nutzap.pubkey} userProfile={userProfile} imageSize={40} />
